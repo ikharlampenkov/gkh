342c34f3286e7f4a2844ff76a9eb6d155e7e8b50
Moris <Moris@Main>
2011-05-16 20:17:22 +0700

    Английские названия документов

--- public/site_gkh/index.php
+++ public/site_gkh/index.php
@@ -2,13 +2,53 @@
 
 include_once $_SERVER['DOCUMENT_ROOT'] . '/allinclud.php';
 
-$page = '';
-
-
 $o_smarty = new simo_smarty();
+$o_user = new share_user_site();
+
+if (isset($_GET['page'])) {
+    $page = $_GET['page'];
+} else {
+    $page = '';
+}
+
+if (isset($_GET['action'])) {
+    $action = $_GET['action'];
+} else {
+    $action = '';
+}
 
 $o_smarty->assign('page', $page);
+$o_smarty->assign('action', $action);
 
-$o_smarty->display('index.tpl');
 
+if ($o_fmuser->isLogin()) {
+    $o_smarty->assign('login', true);
+    $o_smarty->assign('user', simo_session::getVar('login', 'user'));
+    $o_smarty->assign('role', $o_fmuser->getUserRole());
+
+    if (isset($_GET['logout'])) {
+        $o_fmuser->logOut();
+        header("Location: /");
+    }
+
+    if ($o_fmuser->getUserRole() == '') {
+        include_once $_SERVER['DOCUMENT_ROOT'] . '/admin/index.php';
+    } elseif ($o_fmuser->getUserRole() == '') {
+        include_once $_SERVER['DOCUMENT_ROOT'] . '/cabinet/index.php';
+    } else {
+        
+    }
+} else {
+    $o_smarty->assign('login', false);
+    
+    if (isset($_POST['login']) && isset($_POST['psw'])) {
+        if ($o_fmuser->logIn($_POST['login'], $_POST['psw'])) {
+            header("Location: /");
+        } else {
+            $o_smarty->assign('login_fail', '1');
+        }
+    }    
+    
+    $o_smarty->display('index.tpl');
+}
 ?>
